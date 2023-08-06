cfefed6955cf9b4db03ea9a11d01a249fb812fa9
Eyasin81 <eyasina81.com>
2023-08-06 22:10:01 +0600

    add

---
+++ 8_Function/8.1SimpleDefinition.js
@@ -0,0 +1,9 @@
+function test(){
+   //function body
+}
+// Simple function definition
+
+function test(){
+   console.log("hello function");
+}
+test()
---
+++ 8_Function/8.2Return.js
@@ -0,0 +1,11 @@
+// Return Function
+function test(x,y){
+   return x+y
+}
+var e=test(10,20)
+console.log(e);
+function zero(){
+   return "hello Bangladesh"
+}
+var all=zero()
+console.log(all);
