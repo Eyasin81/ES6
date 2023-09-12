23752f16fc52f3c924f1441d10260ec1e1578cf6
Eyasin81 <eyasina81.com>
2023-09-12 08:54:27 +0600

    What is Array

---
+++ 21_Array/21.1_What_is_Array.js
@@ -0,0 +1,15 @@
+
+// what is Array 
+// how to declare array
+
+// Declare Array 
+
+//1. Using Square Brackets (Array Literal Syntax):
+const a=['a','b','c']
+
+//2. Using the Array() Constructor:
+
+const cars = new Array("Ford", "Toyota", "Honda");
+
+// Empty Array:
+const myArray = [];
