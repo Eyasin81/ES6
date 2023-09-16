bd6bf0b7740a04f51642acd243f73d852b3eeb34
Eyasin81 <eyasina81.com>
2023-09-16 23:20:32 +0600

    Slice()

---
+++ 21.2_Creating Subarrays.js
@@ -0,0 +1,13 @@
+/* Creating Subarrays:
+
+slice():
+subarray(): // not supported all browser
+*/
+let myArray=[1,2,3]
+let all=myArray.slice(1,2)
+console.log(all);
+
+let originalArray = [1, 2, 3, 4, 5];
+let slicedArray = originalArray.slice(1, 4);
+
+console.log(slicedArray); // আউটপুট: [2, 3, 4]
