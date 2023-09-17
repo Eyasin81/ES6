3eb66ccb67293df75c437f4911671d5133cab261
Eyasin81 <eyasina81.com>
2023-09-17 16:17:53 +0600

    some()

---
+++ 21.11_some.js
@@ -0,0 +1,8 @@
+// method-3 some
+// অ্যারের কোন সংখ্যা যেটি 3 এর সমান বা বড়, সেটি খুঁজে পেতে
+const hasNumber = numbers.some((number) =>{
+   return number >= 3;
+ });
+ 
+ console.log(hasNumber); // true (যেকোনো সংখ্যা 3 এর সমান বা বড়)
+ 
