1d5b9717ff1c7a874fdd7f35f6c0f2410ddaa707
Eyasin81 <eyasina81.com>
2023-09-17 16:21:55 +0600

    includes()

---
+++ 21.13_includes.js
@@ -0,0 +1,13 @@
+// let numbers=[1,2,3,4]
+// 1 উপাংশটি অ্যারেতে রয়েছে কিনা পরীক্ষা করতে
+const hasNumbers = numbers.includes(1);
+
+console.log(hasNumbers); // true (1 অ্যারেতে রয়েছে)
+// Array  includes() Method
+const str = "Hello, World!";
+console.log(str.includes("World")); // true
+console.log(str.includes("Goodbye")); // false
+
+const arr = [1, 2, 3, 4, 5];
+console.log(arr.includes(3)); // true
+console.log(arr.includes(6)); // false
